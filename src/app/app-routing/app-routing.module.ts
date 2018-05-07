5f52049e2ad0bcf95113b4f9d96c957772581a02
rhamache <ryan.hamacher@stantec.com>
2018-05-07 08:39:34 -0600

    restyling messages to look like chat bubbles

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignUpFormComponent } from '../sign-up-form/sign-up-form.component';
-import { TwitterFormComponent } from '../twitter-form/twitter-form.component'
+import { TwitterFormComponent } from '../twitter-form/twitter-form.component';
 const routes: Routes = [
     {
       path: 'sign-up',
